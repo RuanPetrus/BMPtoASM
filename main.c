efe1443bb48d6a5c3397c63528cbdc90502ddf35
Ruan Petrus <xastroboyx11@gmail.com>
2022-03-17 12:06:10 -0300

    fixed

--- main.c
+++ main.c
@@ -117,13 +117,13 @@ void pixel_array_to_asm(Pixel* pixels, uint32_t width, uint32_t height)
 
 	uint32_t total =  width * height;
 
-	for (uint32_t j = 0; j < height; j++) {
-		for (uint32_t i = 0; i < width; i++) {
-			uint32_t index = width * (height - j - 1) + i;
-			uint8_t color = bmp_to_asm(pixels[index]);
-			fprintf(fptr, "%d,", color);
-		}
-	}
+    for (uint32_t j = 0; j < height; j++) {
+        for (uint32_t i = 0; i < width; i++) {
+            uint32_t index = width * (height - j - 1) + i;
+            uint8_t color = bmp_to_asm(pixels[index]);
+            fprintf(fptr, "%d,", color);
+        }
+    }
 
 	fclose(fptr);
 }
@@ -131,7 +131,7 @@ void pixel_array_to_asm(Pixel* pixels, uint32_t width, uint32_t height)
 int main(int argc, char **argv)
 {
     if (argc < 2) {
-        fprintf(stderr, "Fail to read file\n");
+        fprintf(stderr, "Fail to read image\n");
         usage(stderr);
         exit(1);
     }
@@ -140,6 +140,12 @@ int main(int argc, char **argv)
     char *image_path = argv[1];
 
     char *content = slurp_file(image_path, &content_size);
+
+    if (content == NULL) {
+        fprintf(stderr, "ERROR: not possible to open image: %s\n", image_path);
+        usage(stderr);
+        exit(1);
+    }
 
     uint32_t width, height;
     uint32_t offset_to_pixels;
@@ -152,12 +158,11 @@ int main(int argc, char **argv)
     uint8_t r,g,b;
 
     uint32_t inicial_point = offset_to_pixels;
-	if (inicial_point % 4 != 0) inicial_point += 2;
-    uint32_t final_point = inicial_point + (width * height * 3);
+	uint8_t padding = (4 - ((width * 3) % 4)) % 4;
+    uint32_t final_point = inicial_point + (width * height * 3) + (height * padding);
 
 	Pixel *pixels = malloc(3 * 8 * width * height); 
-
-	uint8_t padding = (4 - ((width * 3) % 4)) % 4;
+    memset(pixels, 0, 3 * 8 * width * height);
 
 	size_t arr_count = 0;
     for (size_t i = inicial_point; i < final_point; i+=3)
@@ -170,24 +175,20 @@ int main(int argc, char **argv)
 			continue;
 		}
 
-        ByteArrayLE_to_uint8((unsigned char *) content, &r, i + 0);
+        ByteArrayLE_to_uint8((unsigned char *) content, &b, i + 0);
         ByteArrayLE_to_uint8((unsigned char *) content, &g, i + 1);
-        ByteArrayLE_to_uint8((unsigned char *) content, &b, i + 2);
+        ByteArrayLE_to_uint8((unsigned char *) content, &r, i + 2);
 
 		pixels[arr_count].red = r;
 		pixels[arr_count].green = g;
 		pixels[arr_count].blue = b;
 		arr_count++;
-		//printf("%d %d %d  j:%zu\n", r, g, b, j);
+        // printf("%d %d %d  j:%zu\n", r, g, b, j);
     }
 
-	// pixel_array_to_asm(pixels, width, height);
-	Pixel pixel;
-	pixel.red = 0;
-	pixel.green = 100;
-	pixel.blue = 25;
-	printf("%d", bmp_to_asm(pixel));
+	pixel_array_to_asm(pixels, width, height);
 
+    free(content);
 
     return 0;
 }
